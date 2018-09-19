3587d1c0c2eef4dce8db9346ba3f65db1e9b38fe
tgobran <tylergobran@shaw.ca>
2018-09-19 14:59:43 -0600

    Continued outlining UI

--- app/src/main/java/com/example/gobran/gobran_feelsbook/MainActivity.java
+++ app/src/main/java/com/example/gobran/gobran_feelsbook/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.gobran.gobran_feelsbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void accessStatistics(View view) {
+        Intent intent = new Intent(this, StatisticsActivity.class);
+        startActivity(intent);
+    }
+
+    public void accessHistory(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
     }
 }
