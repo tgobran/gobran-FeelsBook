73a0cb0440d71b85fcf1ec6cf50bc795e24d3f49
Tyler Gobran <33161763+tgobran@users.noreply.github.com>
2018-10-03 12:58:27 -0600

    Added comments to CommentWriterFragment

--- app/src/main/java/com/example/gobran/gobran_feelsbook/CommentWriterFragment.java
+++ app/src/main/java/com/example/gobran/gobran_feelsbook/CommentWriterFragment.java
@@ -1,3 +1,23 @@
+/*
+ * Class: CommentWriterFragment
+ *
+ * Version: 1.0
+ *
+ * Date: October 3rd, 2018
+ *
+ * Description:
+ *      Handles the creation of a DialogFragment on screen that will allow the 
+ *      editing and creation of comments for emotion records.
+ * Rationale:
+ *      Seperates the operation of creating comments from specific editText views
+ *      in specific activities, allows any class that implements its method to
+ *      recieve a comment be able to allow comment editing easily
+ *      In this sense it allows for greater modularity, with it being possible
+ *      to reuse this code in other projects with commenting.
+ * Outstanding Issues:
+ *     Few issues are prevalent with the current design due to its relative simplicity
+ */
+
 package com.example.gobran.gobran_feelsbook;
 
 import android.app.AlertDialog;
@@ -14,21 +34,28 @@ import android.widget.EditText;
 public class CommentWriterFragment extends DialogFragment {
     CommentWriterFragment.OnCommentSetListener mCallback;
 
+    //Interface a implementaing class must support, so that the returned comment will be handled 
     public interface OnCommentSetListener {
         public void onCommentSet(String comment);
     }
 
+    //Creates the actual dialog residing within the fragment
     @Override
     public @NonNull Dialog onCreateDialog(Bundle savedInstanceState) {
+        
+        //Grabs the view containing the comment editor textEdit, places that as the dialog view
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View commentWriterView = inflater.inflate(R.layout.fragment_comment_writer, null);
+ 
+        //Changes the editText object within the view to present any pre-existing comment
+        //Such comments are passed to the commentWriter via an argument
         final EditText commentEditor = commentWriterView.findViewById(R.id.commentWriterFragment_EditComment);
-
         Bundle commentData = getArguments();
         if (commentData != null) {
             commentEditor.setText(commentData.getString(this.getString(R.string.commentWriterFragment_CommentArgument),""));
         }
 
+        //Builds the dialog using the view provided, sets the positive and negative return buttons to return the sttring or nothing 
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
         dialogBuilder.setView(commentWriterView)
                 .setPositiveButton(R.string.commentWriterFragment_ConfirmButton, new DialogInterface.OnClickListener() {
@@ -44,14 +71,16 @@ public class CommentWriterFragment extends DialogFragment {
                         mCallback.onCommentSet(null);
                     }
                 });
-
+        
+        //Returns the created dialog with its proper editText and its buttons established
         return dialogBuilder.create();
     }
 
+    //Handles the fragment being attached to an activity, ensures that it knows its parentContext which will recieve the
+    //resulting comments that are written
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
         try {
             mCallback = (CommentWriterFragment.OnCommentSetListener) context;
         } catch (ClassCastException e) {
